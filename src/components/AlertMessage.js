4c8bd89b9b26ed44aa5133fdc83ec0ead71dfcc3
latifaghassan <lalooshamayra@hotmail.com>
2021-06-14 12:15:48 +0300

    lab06

---
+++ src/components/AlertMessage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Alert from "react-bootstrap/Alert";
+
+export class AlertMessage extends Component {
+  render() {
+    return (
+      <div>
+        <Alert variant="danger">
+          This is a {this.props.error} alert-check it out!
+        </Alert>
+      </div>
+    );
+  }
+}
+
+export default AlertMessage;
