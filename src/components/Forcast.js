40aace76817d6aa0fb9502628a02bc023a45605f
latifaghassan <lalooshamayra@hotmail.com>
2021-06-15 22:04:39 +0300

    lab07 finished

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 
 export class Forcast extends Component {
   render() {
     return (
-      <div>
-        <p>{this.props.weatherData.temperature}</p>
-        <p>{this.props.weatherData.wather}</p>
-        <p>{this.props.weatherData.date}</p>
-      </div>
+      <>
+        {this.props.weather.map((weatherObj) => {
+          return (
+            <>
+              <ListGroup
+                style={{ width: "30%", marign: "Auto", textAlign: "center" }}
+              >
+                {weatherObj.description}
+              </ListGroup>
+              <ListGroup
+                style={{ width: "30%", margin: "Auto", textAlign: "center" }}
+              >
+                {weatherObj.date}
+              </ListGroup>
+            </>
+          );
+        })}
+      </>
     );
   }
 }
