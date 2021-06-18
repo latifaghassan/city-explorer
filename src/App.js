b87c3b9513066dc7b1e2c45a8130c80ce5b94851
latifaghassan <lalooshamayra@hotmail.com>
2021-06-18 22:01:57 +0300

    lab08

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import SearchForm from "./components/SearchForm";
 import Map from "./components/Map";
 import Forcast from "./components/Forcast";
 import CityData from "./components/CityData";
-import Movies from "./components/Movies";
-
-//---------------------------------------------------------------------------------------------
 
 class App extends React.Component {
   constructor(props) {
@@ -18,18 +15,13 @@ class App extends React.Component {
     this.state = {
       cityName: "",
       cityData: {},
+      displayData: false,
       weatherData: "",
-      moviesData: "",
       lat: "",
       lon: "",
-
-      displayData: false,
-
-      hasError: "",
+      error: false,
     };
   }
-
-  //---------------------------------------------------------------------------------------------
 
   updateCityNameState = (e) => {
     this.setState({
@@ -37,87 +29,57 @@ class App extends React.Component {
     });
   };
 
-  //---------------------------------------------------------------------------------------------
-
   getCityData = async (e) => {
     e.preventDefault();
+
     try {
-      // LOCATION
-      await axios
-        .get(
-          `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${this.state.cityName}&format=json`
-        )
-        .then((reponseData) => {
-          this.setState({
-            cityData: reponseData.data[0],
-            lat: reponseData.data[0].lat,
-            lon: reponseData.data[0].lon,
-          });
-
-          //   // WEATHER
-          axios
-
-            .get(
-              `${process.env.REACT_APP_URL}/weather?&lat=${this.state.lat}&lon=${this.state.lon}`
-            )
-
-            .then((reponseData) => {
-              this.setState({
-                weatherData: reponseData.data,
-                // displayData: true,
-                // we target here the request data // this is the data that we got from the backend
-                alert: false,
-              });
-              // MOVIES
-              axios
-                .get(
-                  `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${this.state.cityName}`
-                )
-                .then((reponseData) => {
-                  this.setState({
-                    weatherData: reponseData.data,
-                    displayData: true,
-                    alert: false,
-                  });
-                });
-            });
-          // weatherData: reponseData.data.results,
-        });
-
-      //   //--------------------------------------------------------------------------------------------
-    } catch (error) {
+      const axiosResponse = await axios.get(
+        `https://us1.locationiq.com/v1/search.php?key=pk.d36871f015649f915282f374cff76628&city=${this.state.cityName}&format=json`
+      );
       this.setState({
-        hasError: error.message,
-        alert: true,
+        cityData: axiosResponse.data[0],
+        lat: axiosResponse.data[0].lat,
+        lon: axiosResponse.data[0].lon,
+        error: false,
+      });
+      const myApiResponse = await axios.get(
+        `${process.env.REACT_APP_URL}/weather?lon=${this.state.lon}&lat=${this.state.lat}`
+      );
+      this.setState({
+        weatherData: myApiResponse.data,
+        displayData: true,
+      });
+    } catch {
+      this.setState({
+        error: true,
       });
     }
   };
-  // --------------------------------------------------------------------------------------------
-
   render() {
     return (
       <div>
         <Header />
-        {this.state.alert && <AlertMessage hasError={this.state.error} />}
+
         <SearchForm
           getCityData={this.getCityData}
           updateCityNameState={this.updateCityNameState}
         />
-        {this.state.displayData && (
-          <div>
-            <Map cityData={this.state.cityData} />
-            <CityData cityData={this.state.cityData} />
-            <Forcast weather={this.state.weatherData} />
-            <Movies movies={this.state.moviesData} />
-          </div>
-        )}
+
+        {(this.state.error && <AlertMessage />) ||
+          (this.state.displayData && (
+            <div>
+              <Map cityData={this.state.cityData} />
+
+              <CityData cityData={this.state.cityData} />
+
+              <Forcast weather={this.state.weatherData} />
+            </div>
+          ))}
+
         <Footer />
       </div>
     );
   }
 }
-export default App;
 
-// 3000 for frontend (react)
-// 3002 for backend
-// they shouldn't be same.
+export default App;
