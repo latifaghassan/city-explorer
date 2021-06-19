a649ef17fd4ec9488529696ba27809a70fe6250d
latifaghassan <lalooshamayra@hotmail.com>
2021-06-19 03:18:18 +0300

    called the components inside App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AlertMessage from "./components/AlertMessage";
+import SearchForm from "./components/SearchForm";
+import CityData from "./components/CityData";
+import Map from "./components/Map";
+
 import React, { Component } from "react";
 
 export class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Header />
+        <SearchForm />
+        <CityData />
+        <Map />
+        <AlertMessage />
+        <Footer />
+      </div>
+    );
   }
 }
 
