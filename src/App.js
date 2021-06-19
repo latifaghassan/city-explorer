ef49e85c9edf092f4e1ebe51b492984f26ac3a2b
latifaghassan <lalooshamayra@hotmail.com>
2021-06-19 04:17:55 +0300

    finished lab 07

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// import weatherData from "./assets/weather.json";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AlertMessage from "./components/AlertMessage";
 import SearchForm from "./components/SearchForm";
 import CityData from "./components/CityData";
 import Map from "./components/Map";
+import Forcast from "./components/Forcast";
 
 //------------------------------------------------
 export class App extends Component {
@@ -18,6 +19,10 @@ export class App extends Component {
       cityData: {},
       displayData: false,
       error: false,
+      weatherData: "",
+
+      lat: "",
+      lon: "",
     };
   }
   //------------------------------------------------
@@ -26,17 +31,28 @@ export class App extends Component {
       cityName: e.target.value,
     });
   };
+  //------------------------------------------------
 
   getCityData = async (e) => {
     e.preventDefault();
     try {
       const axiosResponse = await axios.get(
-        `https://us1.locationiq.com/v1/search.php?key=pk.d36871f015649f915282f374cff76628&city=${this.state.cityName}&format=json`
+        `https://us1.locationiq.com/v1/search.php?key=pk.83c86caa48f11d093c8138a3a3fc4185&city=${this.state.cityName}&format=json`
       );
+
       this.setState({
         cityData: axiosResponse.data[0],
-        displayData: true,
+        lat: axiosResponse.data[0].lat,
+        lon: axiosResponse.data[0].lon,
         error: false,
+      });
+      const myApiResponse = await axios.get(
+        `${process.env.REACT_APP_URL}/weather?lon=${this.state.lon}&lat=${this.state.lat}`
+      );
+
+      this.setState({
+        weatherData: myApiResponse.data,
+        displayData: true,
       });
     } catch {
       this.setState({ error: true });
@@ -58,6 +74,8 @@ export class App extends Component {
               <Map cityData={this.state.cityData} />
 
               <CityData cityData={this.state.cityData} />
+
+              <Forcast weather={this.state.weatherData} />
             </div>
           ))}
 
---
+++ src/components/Forcast.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+
+export class Forcast extends Component {
+  render() {
+    return (
+      <>
+        {this.props.weather.map((weatherObj) => {
+          return (
+            <>
+              <ListGroup
+                style={{ width: "30%", marign: "Auto", textAlign: "center" }}
+              >
+                {weatherObj.description}
+              </ListGroup>
+              <ListGroup
+                style={{ width: "30%", margin: "Auto", textAlign: "center" }}
+              >
+                {weatherObj.date}
+              </ListGroup>
+            </>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default Forcast;
