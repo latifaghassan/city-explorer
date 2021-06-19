8b01ed2793f8e09c0dca37dae5153124f469ce28
latifaghassan <lalooshamayra@hotmail.com>
2021-06-19 12:48:44 +0300

    delete things

--- src/components/CityData.js
+++ src/components/CityData.js
@@ -4,12 +4,9 @@ export class CityData extends Component {
   render() {
     return (
       <div>
-        <p className="display_name">
-          {" "}
-          City Name :{this.props.cityData.display_name}
-        </p>
-        <p className="lat">Lat : {this.props.cityData.lat}</p>
-        <p className="lon"> Lon :{this.props.cityData.lon}</p>
+        <p className="display_name">{this.props.cityData.display_name}</p>
+        <p className="lat">{this.props.cityData.lat}</p>
+        <p className="lon">{this.props.cityData.lon}</p>
       </div>
     );
   }
