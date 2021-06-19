4b4341d5f6c349b54c2e26f7910a831caeb80ff7
latifaghassan <lalooshamayra@hotmail.com>
2021-06-19 03:09:32 +0300

    added the essential elements

--- src/App.js
+++ src/App.js
@@ -1,86 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cityName: "",
-      cityData: {},
-      displayData: false,
-    };
-  }
+import React, { Component } from "react";
 
-  updateCityNameState = (e) => {
-    this.setState({
-      cityName: e.target.value,
-    });
-  };
-
-  getCityData = async (e) => {
-    e.preventDefault();
-    try {
-      const axiosResponse = await axios.get(
-        `https://us1.locationiq.com/v1/search.php?key=pk.d36871f015649f915282f374cff76628&city=${this.state.cityName}&format=json`
-      );
-      this.setState({
-        cityData: axiosResponse.data[0],
-        displayData: true,
-        error: false,
-      });
-    } catch {
-      this.setState({ error: true });
-    }
-  };
-
+export class App extends Component {
   render() {
-    return (
-      <div>
-        <Header />
-
-        <Form onSubmit={this.getCityData} className="form">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>City Name:</Form.Label>
-            <Form.Control
-              onChange={this.updateCityNameState}
-              type="text"
-              placeholder="write a name of a city"
-            />
-          </Form.Group>
-
-          <Form.Group
-            className="mb-3"
-            controlId="formBasicCheckbox"
-          ></Form.Group>
-          <Button className="button" variant="primary" type="submit">
-            Explore!
-          </Button>
-        </Form>
-
-        {(this.state.error && <p className="error">ERROR!</p>) ||
-          (this.state.displayData && (
-            <div>
-              <p className="city">{this.state.cityData.display_name}</p>
-              <p className="city">{this.state.cityData.lat}</p>
-              <p className="city">{this.state.cityData.lon}</p>
-
-              <img
-                className="map"
-                src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`}
-                alt=""
-              />
-            </div>
-          ))}
-
-        <Footer />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
